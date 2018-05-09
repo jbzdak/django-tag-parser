98e085421d793c842559dd4a962b113bcd932457
Jacek Bzdak <jbzdak@gmail.com>
2018-05-09 18:07:42 +0200

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
 
   setup(
     name='simple-django-tag-parser',
-    version='0.2.0',
+    version='0.2.1',
     packages=packages,
     license='MIT',
     author='Jacek Bzdak',
